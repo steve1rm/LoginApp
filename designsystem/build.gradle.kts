780409811e54af80b39b1a39724ef0a7cce56515
Steve <steve@kitkat@gmail.com>
2024-07-18 20:57:15 +0700

    Add sign in button

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    debugImplementation(libs.ui.tooling)
 }
