7d894135a1bdc6ccea1c0caa23bc6014efa905ef
Steve <steve@kitkat@gmail.com>
2024-07-20 23:10:41 +0700

    Adds test for UI screen login

--- authentication/presentation/build.gradle.kts
+++ authentication/presentation/build.gradle.kts
@@ -53,14 +53,14 @@ dependencies {
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
 
-  /*  debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.ui.test.manifest)
     androidTestImplementation(libs.junit5.api)
     androidTestImplementation(libs.junit5.params)
     runtimeOnly(libs.junit5.engine)
     androidTestImplementation(libs.assertk)
     androidTestImplementation(libs.turbine)
     androidTestImplementation(libs.coroutines.test)
-    androidTestImplementation(libs.junit5.android.test.compose)*/
+    androidTestImplementation(libs.junit5.android.test.compose)
 
     testImplementation(libs.junit5.api)
     testImplementation(libs.junit5.params)
